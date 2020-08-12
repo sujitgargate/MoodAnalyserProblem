c936b11834ad8f569353b847bc213662af12d1f1
sujit gargate <sujitgargate5893@gmail.com>
2020-08-12 20:52:03 +0530

    Refactored : Test Case - Sad Message Returns SAD

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -1,18 +1,19 @@
 import org.graalvm.compiler.lir.profiling.MoveProfilingPhase_OptionDescriptors;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
 
    @Test
-   public void check_HappyMood() {
+   public void givenMessage_Should_Return_Happy() {
       MoodAnalyser moodAnalyser = new MoodAnalyser();
       String moodState = moodAnalyser.analyseMood("This is a Happy Message");
       Assert.assertEquals("HAPPY", moodState);
    }
 
    @Test
-   public void check_SadMood() {
+   public void givenMessage_Should_Return_Sad() {
       MoodAnalyser moodAnalyser = new MoodAnalyser();
       String moodState = moodAnalyser.analyseMood("This is a Sad Message");
       Assert.assertEquals("SAD", moodState);
